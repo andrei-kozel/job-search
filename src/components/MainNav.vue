6d3717c96b3cb353528bc2f486d35207add5e38c
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-05 16:53:26 +0100

    navigation component base

---
+++ src/components/MainNav.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 h-16 w-full bg-white">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+  data() {
+    return {
+      company: "Jobs & Careers",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
