14447da1d3db870af8ec658097bce6f8de72dbcc
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-05 19:01:33 +0100

    menu items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,22 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a class="flex h-full items-center text-xl" href="/">{{ company }}</a>
+        <a class="flex h-full items-center text-xl font-bold" href="/">{{
+          company
+        }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="item in menuItems"
+              :key="item.title"
+              class="ml-9 h-full first:ml-0 hover:text-gray-400"
+            >
+              <a :href="item.url" class="flex h-full items-center py-2.5">{{
+                item.title
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +31,14 @@ export default {
   data() {
     return {
       company: "Jobs & Careers",
+      menuItems: [
+        { title: "Teams", url: "/" },
+        { title: "Location", url: "/" },
+        { title: "Life at J&C", url: "/" },
+        { title: "How we hire", url: "/" },
+        { title: "Students", url: "/" },
+        { title: "Jobs", url: "/" },
+      ],
     };
   },
 };
