eb1cea6e8fec904438314454e54386470d26d8f5
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-05 16:24:43 +0100

    fix vue files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue, ts, js}"],
   theme: {
     extend: {
       fontFamily: {
