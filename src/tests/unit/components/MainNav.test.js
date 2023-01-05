51724434e398d99c64d00e972a3215089aff388b
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-05 19:01:43 +0100

    menu items

--- src/tests/unit/components/MainNav.test.js
+++ src/tests/unit/components/MainNav.test.js
@@ -7,4 +7,20 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Jobs & Careers");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Location",
+      "Life at J&C",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
