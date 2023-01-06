5a796652591b53adbe8ad82e92c7365376a4bb5d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 14:45:10 +0100

    init subnav tests

---
+++ tests/unit/components/SubNav.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/vue";
+import SubNav from "@/components/SubNav.vue";
+
+describe("SubNav", () => {
+  /* This is a test that checks if the job count is displayed when the user is on the jobs page. */
+  describe("when user is on Jobs page", () => {
+    it("displays job count", () => {
+      render(SubNav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: true,
+          };
+        },
+      });
+
+      const jobCount = screen.getByText("1653");
+      expect(jobCount).toBeInTheDocument();
+    });
+  });
+
+  /* Testing that the job count is not displayed when the user is not on the jobs page. */
+  describe("when user is not on Jobs page", () => {
+    it("does NOT displays job count", () => {
+      render(SubNav, {
+        global: {
+          stubs: {
+            FontAwesomeIcon: true,
+          },
+        },
+        data() {
+          return {
+            onJobResultsPage: false,
+          };
+        },
+      });
+    });
+
+    const jobCount = screen.queryByText("1653");
+    expect(jobCount).not.toBeInTheDocument();
+  });
+});
