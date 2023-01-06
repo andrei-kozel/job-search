38087f2f42bcde80167a8b67560da5f9e97153de
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 12:05:37 +0100

    button tests

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/vue";
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("ActionButton", () => {
+  /* Testing that the button is in the document. */
+  it("renders text", () => {
+    render(ActionButton, {
+      props: {
+        text: "Click me",
+        type: "primary",
+      },
+    });
+
+    const button = screen.getByRole("button", {
+      name: /click me/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  /* Testing that the button has the class primary. */
+  it("applies one of several styles to button", () => {
+    render(ActionButton, {
+      props: {
+        text: "Click me",
+        type: "primary",
+      },
+    });
+
+    const button = screen.getByRole("button", {
+      name: /click me/i,
+    });
+    expect(button).toHaveClass("primary");
+  });
+});
