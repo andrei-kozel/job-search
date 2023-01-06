422f4662a263227242696741ee1da202ad042efd
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 20:01:22 +0100

    hero component init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <MainNav />
+  <TheHero />
 </template>
 
 <script>
 import MainNav from "@/components/MainNav.vue";
+import TheHero from "@/components/TheHero.vue";
 
 export default {
   name: "App",
-  components: { MainNav },
+  components: { MainNav, TheHero },
 };
 </script>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>Hero component</main>
+</template>
+
+<script>
+export default {
+  name: "TheHero",
+};
+</script>
