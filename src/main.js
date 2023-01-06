c58455b4c451b7830e6244fc7d4a9d8d159af483
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 14:42:43 +0100

    global component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
-import App from "@/App.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import "@/index.css";
+import App from "@/App.vue";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("FontAwesomeIcon", FontAwesomeIcon).mount("#app");
