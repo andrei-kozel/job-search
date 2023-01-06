10694b79486c2494724441740db48ce9e319ae9b
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 12:06:26 +0100

    profile image

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    :src="image"
+    class="h-8 w-8 rounded-full object-contain"
+    alt="User profile image"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      image: "https://i.pravatar.cc/300",
+    };
+  },
+};
+</script>
