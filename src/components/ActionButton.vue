4612af0a01d6ae7ab8fd64b434fc66aaab2498f3
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 12:06:19 +0100

    action button component

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button :class="buttonClass">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: {
+    text: { type: String, required: true },
+    type: {
+      type: String,
+      required: true,
+      default: "primary",
+      validator(value) {
+        return ["primary", "secondary"].includes(value);
+      },
+    },
+  },
+  computed: {
+    buttonClass() {
+      return { [this.type]: true };
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  @apply rounded-md  px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
