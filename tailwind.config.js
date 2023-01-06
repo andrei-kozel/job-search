e985e77b1b0a7360c1fbbced17443642e0298c47
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-06 12:06:05 +0100

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,13 @@ module.exports = {
       colors: {
         "brand-gray-1": "#dadce0",
         "brand-blue-1": "#1967d2",
+        "brand-blue-2": "#4285f4",
         "brand-green-1": "#137333",
       },
+      boxShadow: {
+        blue: "0 0 3px 3px #4285f4",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
 };
