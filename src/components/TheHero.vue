8a2e61ec7ca3fe22287bf7d459580042c5dcf9af
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-07 16:27:13 +0100

    hero image

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -7,7 +7,14 @@
           <TheHeadline />
           <JobSearchForm />
         </div>
-        <div class="col-span-5 col-start-7"></div>
+        <div
+          class="col-span-5 col-start-7 self-center justify-self-center p-10"
+        >
+          <img
+            class="h-90 w-90 rounded-3xl object-contain"
+            src="hero_image.jpeg"
+          />
+        </div>
         <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
