32e9076f1c50fecebfc0aa1374f6bead6b0c4364
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-07 16:27:30 +0100

    fix rounded corners

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -28,11 +28,11 @@ export default {
 
 <style scoped>
 button {
-  @apply rounded-md  px-5 py-3 font-medium;
+  @apply px-5 py-3 font-medium;
 }
 
 .primary {
-  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+  @apply rounded-md bg-brand-blue-1 text-white hover:shadow-blue;
 }
 
 .secondary {
