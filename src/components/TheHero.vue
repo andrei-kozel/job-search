09cb761a0edd323dc7c8c328eb649965deed4645
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-07 15:50:55 +0100

    more cleanup

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,14 +1,14 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+    <section class="flex h-screen flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1">1 col</div>
+        <div class="col-span-1 col-start-1"></div>
         <div class="col-span-5 col-start-2">
           <TheHeadline />
           <JobSearchForm />
         </div>
-        <div class="col-span-5 col-start-7">5 cols</div>
-        <div class="col-span-1 col-start-12">1 col</div>
+        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
   </main>
