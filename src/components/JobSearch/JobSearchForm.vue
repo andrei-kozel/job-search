c7678b3f85abb80970f0a8d47b1716b65af64b86
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-07 20:14:00 +0100

    TextInput component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,25 +6,15 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineere"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <TextInput v-model="role" placeholder="Software engineere" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
         >in</span
       >
       <div class="relative flex h-full flex-1 items-center pl-3">
-        <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <label class="absolute left-3 -top-10">Where?</label>
+        <TextInput v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
 
@@ -34,11 +24,13 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 
 export default {
   name: "JobSearchForm",
   components: {
     ActionButton,
+    TextInput,
   },
   data() {
     return {
