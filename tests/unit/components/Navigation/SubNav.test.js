a5a695fed0aada69debc5826bf68b738c5824597
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-08 12:38:23 +0100

    refactor

--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -2,23 +2,26 @@ import { render, screen } from "@testing-library/vue";
 import SubNav from "@/components/Navigation/SubNav.vue";
 
 describe("SubNav", () => {
+  const renderSubNav = (path) => {
+    const $route = {
+      name: path,
+    };
+    render(SubNav, {
+      global: {
+        mocks: {
+          $route,
+        },
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+  };
+
   /* This is a test that checks if the job count is displayed when the user is on the jobs page. */
   describe("when user is on Jobs page", () => {
     it("displays job count", () => {
-      const $route = {
-        name: "JobResults",
-      };
-      render(SubNav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
-
+      renderSubNav("JobResults");
       const jobCount = screen.getByText("1653");
       expect(jobCount).toBeInTheDocument();
     });
@@ -27,22 +30,9 @@ describe("SubNav", () => {
   /* Testing that the job count is not displayed when the user is not on the jobs page. */
   describe("when user is not on Jobs page", () => {
     it("does NOT displays job count", () => {
-      const $route = {
-        name: "Home",
-      };
-      render(SubNav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
+      renderSubNav("Home");
+      const jobCount = screen.queryByText("1653");
+      expect(jobCount).not.toBeInTheDocument();
     });
-
-    const jobCount = screen.queryByText("1653");
-    expect(jobCount).not.toBeInTheDocument();
   });
 });
