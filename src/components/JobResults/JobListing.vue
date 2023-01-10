1b4c404f4f878c7973e7dc97a73b1c89775a0e83
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-10 19:13:53 +0100

    render data

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -1,19 +1,27 @@
 <template>
   <li class="mb-7">
     <router-link
-      to="/jobs/results/1"
+      :to="jobPageLink"
       class="bolder-solid mx-auto block rounded border border-brand-gray-2 bg-white hover:shadow-gray"
     >
       <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
         <h2 class="mb-2 text-2xl">
-          Engineering Manager, Framework Software, Android Automotive
+          {{ job.title }}
         </h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>Bobo</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>San Francisco, CA, USA</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                <span>{{ location }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -22,21 +30,17 @@
           <h3 class="mt-1 mb-2">Qualifications:</h3>
           <div>
             <ul class="list-disc pl-8">
-              <li>
-                Bachelor’s degree in Computer Science, Engineering or equivalent
-                practical experience.
-              </li>
-              <li>Experience coding in C++.</li>
-              <li>Experience in people management.</li>
-              <li>
-                Experience with Embedded Systems and the Android platform and
-                ecosystem.
+              <li
+                v-for="qualification in job.minimumQualifications"
+                :key="qualification"
+              >
+                {{ qualification }}
               </li>
             </ul>
           </div>
         </div>
         <div class="mt-2 text-center">
-          <router-link to="/jobs/results/1" class="text-brand-blue-1"
+          <router-link :to="jobPageLink" class="text-brand-blue-1"
             >Expand</router-link
           >
         </div>
@@ -48,5 +52,16 @@
 <script>
 export default {
   name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    jobPageLink() {
+      return `/jobs/results/${this.job.id}`;
+    },
+  },
 };
 </script>
