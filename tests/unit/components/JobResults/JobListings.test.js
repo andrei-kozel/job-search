81c5504f2c25e5a0fb582477e5cf313362e1b5f6
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 08:12:57 +0100

    refactor

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -7,21 +7,37 @@ import JobListings from "@/components/JobResults/JobListings.vue";
 vi.mock("axios");
 
 describe("JobListings", () => {
-  it("fetches jobs", () => {
-    axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
   });
 
-  it("displays max of 10 jobs", async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+  const renderJobListngs = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route,
+        },
         stubs: {
           RouterLink: RouterLinkStub,
         },
       },
     });
+  };
+
+  it("fetches jobs", () => {
+    axios.get.mockResolvedValue({ data: [] });
+    const $route = createRoute();
+    renderJobListngs($route);
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("displays max of 10 jobs", async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = createRoute({ page: "1" });
+    renderJobListngs($route);
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
   });
