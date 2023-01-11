40de2fa21817550e1e766762098625eaf7da998d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 09:46:03 +0100

    smooth scroll

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, behavior: "smooth" };
+  },
 });
 
 export default router;
