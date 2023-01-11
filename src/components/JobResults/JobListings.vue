08eb609e16e684d9cb95bb80638ccafc6d0c2f40
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 09:45:53 +0100

    page pagination added

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,8 +1,28 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing v-for="job in jobs" :key="job.id" :job="job" />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
+    <div class="max-auto mt-8">
+      <div class="flex flex-row flex-nowrap">
+        <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -19,6 +39,26 @@ export default {
     return {
       jobs: [],
     };
+  },
+  computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || "1");
+    },
+    previousPage() {
+      const prevPage = this.currentPage - 1;
+      return prevPage >= 1 ? prevPage : undefined;
+    },
+    nextPage() {
+      const nextPage = this.currentPage + 1;
+      const maxPage = this.jobs.length / 10;
+      return nextPage <= maxPage ? nextPage : undefined;
+    },
+    displayedJobs() {
+      const page = this.currentPage;
+      const firstjobIndex = (page - 1) * 10;
+      const lastJobIndex = page * 10;
+      return this.jobs.slice(firstjobIndex, lastJobIndex);
+    },
   },
   async mounted() {
     const response = await axios.get("http://localhost:3000/jobs");
