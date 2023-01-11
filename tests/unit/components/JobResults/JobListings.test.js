418c8149059c338c2b32f185b96e39f76e3a172b
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 08:03:58 +0100

    fix: render 10 jobs

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -13,7 +13,7 @@ describe("JobListings", () => {
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
-  it("creates a job listing for every job", async () => {
+  it("displays max of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
     render(JobListings, {
       global: {
@@ -23,6 +23,6 @@ describe("JobListings", () => {
       },
     });
     const jobListings = await screen.findAllByRole("listitem");
-    expect(jobListings).toHaveLength(15);
+    expect(jobListings).toHaveLength(10);
   });
 });
