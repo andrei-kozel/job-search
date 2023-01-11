31bb172d38067239eb5c2a5a9de23647ff98e778
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 11:58:41 +0100

    userStore tests

---
+++ tests/unit/stores/user.test.js
@@ -0,0 +1,28 @@
+import { createPinia, setActivePinia } from "pinia";
+
+import { useUserStore } from "@/stores/user";
+
+describe("state", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it("keeps track if user is logged in", () => {
+    const store = useUserStore();
+    expect(store.isLoggedIn).toBe(false);
+  });
+});
+
+describe("actions", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("loginUser", () => {
+    it("logs the user in", () => {
+      const store = useUserStore();
+      store.loginUser();
+      expect(store.isLoggedIn).toBe(true);
+    });
+  });
+});
