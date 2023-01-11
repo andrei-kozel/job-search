b62848d8215160177ceffdd2f302c36a3e7d6be5
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-11 10:22:24 +0100

    new tests

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -41,4 +41,40 @@ describe("JobListings", () => {
     const jobListings = await screen.findAllByRole("listitem");
     expect(jobListings).toHaveLength(10);
   });
+
+  describe("when params exclde page number", () => {
+    it("displays page number 1", () => {
+      const $route = createRoute({ page: undefined });
+      renderJobListngs($route);
+      expect(screen.getByText("Page 1")).toBeInTheDocument();
+    });
+  });
+
+  describe("hen params includes page number", () => {
+    it("displays pag number", () => {
+      const $route = createRoute({ page: "3" });
+      renderJobListngs($route);
+      expect(screen.getByText("Page 3")).toBeInTheDocument();
+    });
+  });
+
+  describe("when user on first page", () => {
+    it("does not show link to the prev page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: "1" });
+      renderJobListngs($route);
+      await screen.findAllByRole("listitem");
+      const prevLink = screen.queryByRole("link", { name: /previous/i });
+      expect(prevLink).not.toBeInTheDocument();
+    });
+
+    it("shows link to the next page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const $route = createRoute({ page: "1" });
+      renderJobListngs($route);
+      await screen.findAllByRole("listitem");
+      const nextLink = screen.queryByRole("link", { name: /next/i });
+      expect(nextLink).toBeInTheDocument();
+    });
+  });
 });
