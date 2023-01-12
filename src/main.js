ff3c7bd7bfe0feee5a3eedb1ae1549d09050ff57
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-12 11:54:12 +0100

    register more icons

--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@/index.css";
 import router from "@/router";
 import App from "@/App.vue";
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 
 const pinia = createPinia();
 
