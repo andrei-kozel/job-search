1b3bdbbb4f8b7a00c97a0692d77925b22d5e5c19
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-12 11:54:49 +0100

    tbc

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -2,12 +2,24 @@
   <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Job Filter Component
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filter" type="secondary" />
+        </div>
+      </div>
+      <collapsible-accordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
 export default {
   name: "JobFilterSidebar",
+  components: { ActionButton, CollapsibleAccordion },
 };
 </script>
