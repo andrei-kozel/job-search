4ed8b66fefa9e049a9592e06a9ffc190e056ce20
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-12 11:54:18 +0100

    init

---
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="border-b border-solid border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">Organisation</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+
+    <div v-if="isOpen" class="mt-5 w-full">Child</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CollapsibleAccordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
+</script>
