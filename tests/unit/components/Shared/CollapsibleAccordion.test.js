c0ae9220f90d47e005ba76ee91335c17f4d970ff
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-13 18:46:34 +0100

    test accordeon component

---
+++ tests/unit/components/Shared/CollapsibleAccordion.test.js
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
+describe("CollapsibleAccordion", () => {
+  const renderCollapsibleAccordion = (config = {}) => {
+    render(CollapsibleAccordion, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+
+      ...config,
+    });
+  };
+  it("renders child content", async () => {
+    const props = { header: "My Category" };
+    const slots = {
+      default: "<h3>My nested child</h3>",
+    };
+    renderCollapsibleAccordion({ props, slots });
+    expect(screen.queryByText("My nested child")).not.toBeInTheDocument();
+    const button = screen.getByRole("button", { name: /my category/i });
+    await userEvent.click(button);
+    expect(screen.getByText("My nested child")).toBeInTheDocument();
+  });
+
+  describe("when parent does not provide custom child content", () => {
+    it("renders default content", async () => {
+      const props = { header: "My Category" };
+      const slots = {};
+      renderCollapsibleAccordion({ props, slots });
+
+      const button = screen.getByRole("button", { name: /my category/i });
+      await userEvent.click(button);
+      expect(
+        screen.getByText("Whoops, somebody forgot to populate me")
+      ).toBeInTheDocument();
+    });
+  });
+});
