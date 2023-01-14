ff94c793f8244c1caed3368db6e39be1da3ee335
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-14 20:13:52 +0100

    first commit

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,23 @@
+<template>
+  <header-component>
+    <template #title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template #subtitle>
+      <h2 class="my-4 w-full text-base font-light">
+        It's awesome working here. Why don't you come and join us?
+      </h2>
+    </template>
+  </header-component>
+</template>
+
+<script>
+import HeaderComponent from "@/components/Shared/HeaderContainer.vue";
+
+export default {
+  name: "TeamsView",
+  components: {
+    HeaderComponent,
+  },
+};
+</script>
