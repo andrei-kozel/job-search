995400719f554c1ed71cb7ed9b3d03774792da6d
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-15 11:26:55 +0100

    renders data from pinia store

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue
@@ -2,21 +2,16 @@
   <collapsible-accordion class="mt-5" header="Organizations">
     <fieldset>
       <ul class="flex flex-row flex-wrap">
-        <li class="h-8 w-1/2">
-          <input id="VueTube" type="checkbox" class="mr-3" />
-          <label>Placeholder 1</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="VueTube" type="checkbox" class="mr-3" />
-          <label>Placeholder 2</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="VueTube" type="checkbox" class="mr-3" />
-          <label>Placeholder 3</label>
-        </li>
-        <li class="h-8 w-1/2">
-          <input id="VueTube" type="checkbox" class="mr-3" />
-          <label>Placeholder 4</label>
+        <li v-for="org in UNIQUE_ORGANIZATIONS" :key="org" class="h-8 w-1/2">
+          <input
+            :id="org"
+            v-model="selectedOrganizations"
+            :value="org"
+            type="checkbox"
+            class="mr-3"
+            @change="selectOrganizations"
+          />
+          <label>{{ org }}</label>
         </li>
       </ul>
     </fieldset>
@@ -24,12 +19,29 @@
 </template>
 
 <script>
+import { mapState, mapActions } from "pinia";
+import { useJobsStore, UNIQUE_ORGANIZATIONS } from "@/stores/jobs";
+import { useUserStore, ADD_SELECTED_ORGANIZATIONS } from "@/stores/user";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 
 export default {
   name: "JoBFilterSdebarOrganization",
   components: {
     CollapsibleAccordion,
+  },
+  data() {
+    return {
+      selectedOrganizations: [],
+    };
+  },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS]),
+  },
+  methods: {
+    ...mapActions(useUserStore, [ADD_SELECTED_ORGANIZATIONS]),
+    selectOrganizations() {
+      this.ADD_SELECTED_ORGANIZATIONS(this.selectedOrganizations);
+    },
   },
 };
 </script>
