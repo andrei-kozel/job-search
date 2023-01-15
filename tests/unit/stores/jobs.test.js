0634444fea7a8ca4d6d8ea0cf69b70702f90e861
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-15 11:52:13 +0100

    test filtered jobs

--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -2,6 +2,8 @@ import { createPinia, setActivePinia } from "pinia";
 import axios from "axios";
 
 import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
+import { describe } from "vitest";
 
 vi.mock("axios");
 
@@ -46,6 +48,45 @@ describe("getters", () => {
       ];
       const result = store.UNIQUE_ORGANIZATIONS;
       expect(result).toEqual(new Set(["Google", "Amazon"]));
+    });
+  });
+
+  describe("FILTERED_JOBS_BY_ORGANIZATIONS", () => {
+    it("identifies jobs that are associated with the given organizations", () => {
+      const jobsStore = useJobsStore();
+      jobsStore.jobs = [
+        { organization: "Google" },
+        { organization: "Amazon" },
+        { organization: "Microsoft" },
+      ];
+      const userStore = useUserStore();
+      userStore.selectedOrganizations = ["Google", "Microsoft"];
+
+      const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS;
+      expect(result).toEqual([
+        { organization: "Google" },
+        { organization: "Microsoft" },
+      ]);
+    });
+
+    describe("when the user has not selectaed any organization", () => {
+      it("returns all jobs", () => {
+        const jobsStore = useJobsStore();
+        jobsStore.jobs = [
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" },
+        ];
+        const userStore = useUserStore();
+        userStore.selectedOrganizations = [];
+
+        const result = jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS;
+        expect(result).toEqual([
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Microsoft" },
+        ]);
+      });
     });
   });
 });
