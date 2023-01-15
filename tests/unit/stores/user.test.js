4b09657a9fcb53a663d79fae1cb28206a3906188
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-15 11:28:07 +0100

    test selected orgs

--- tests/unit/stores/user.test.js
+++ tests/unit/stores/user.test.js
@@ -11,6 +11,11 @@ describe("state", () => {
     const store = useUserStore();
     expect(store.isLoggedIn).toBe(false);
   });
+
+  it("stores organizations that the user would like to filter jobs by", () => {
+    const store = useUserStore();
+    expect(store.selectedOrganizations).toEqual([]);
+  });
 });
 
 describe("actions", () => {
@@ -23,6 +28,14 @@ describe("actions", () => {
       const store = useUserStore();
       store.loginUser();
       expect(store.isLoggedIn).toBe(true);
+    });
+  });
+
+  describe("ADD_SELECTED_ORGANIZATIONS", () => {
+    it("updates organizations the user chosen to filter jobs by", () => {
+      const store = useUserStore();
+      store.ADD_SELECTED_ORGANIZATIONS(["Org1", "Org2"]);
+      expect(store.selectedOrganizations).toEqual(["Org1", "Org2"]);
     });
   });
 });
