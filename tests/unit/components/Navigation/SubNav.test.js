4cc78962b629e493c0dc9ff2b44bea84317eca5e
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-18 20:37:18 +0100

    fix tests after refactor

--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -1,23 +1,21 @@
 import { render, screen } from "@testing-library/vue";
 import { createTestingPinia } from "@pinia/testing";
 import { useJobsStore } from "@/stores/jobs";
+import { useRoute } from "vue-router";
+
+vi.mock("vue-router");
 
 import SubNav from "@/components/Navigation/SubNav.vue";
+import { vi } from "vitest";
 
 describe("SubNav", () => {
-  const renderSubNav = (path) => {
+  const renderSubNav = () => {
     const pinia = createTestingPinia();
     const jobsStore = useJobsStore();
 
-    const $route = {
-      name: path,
-    };
     render(SubNav, {
       global: {
         plugins: [pinia],
-        mocks: {
-          $route,
-        },
         stubs: {
           FontAwesomeIcon: true,
         },
@@ -30,9 +28,10 @@ describe("SubNav", () => {
   /* This is a test that checks if the job count is displayed when the user is on the jobs page. */
   describe("when user is on Jobs page", () => {
     it("displays job count", async () => {
-      const { jobsStore } = renderSubNav("JobResults");
+      useRoute.mockReturnValue({ name: "JobResults" });
+      const { jobsStore } = renderSubNav();
       const numberOfJobs = 16;
-      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({});
+      jobsStore.FILTERED_JOBS = Array(numberOfJobs).fill({});
       const jobCount = await screen.findByText(numberOfJobs);
       expect(jobCount).toBeInTheDocument();
     });
@@ -41,9 +40,10 @@ describe("SubNav", () => {
   /* Testing that the job count is not displayed when the user is not on the jobs page. */
   describe("when user is not on Jobs page", () => {
     it("does NOT displays job count", () => {
-      const { jobsStore } = renderSubNav("Home");
+      useRoute.mockReturnValue({ name: "Home" });
+      const { jobsStore } = renderSubNav();
       const numberOfJobs = 16;
-      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(numberOfJobs).fill({});
+      jobsStore.FILTERED_JOBS = Array(numberOfJobs).fill({});
       const jobCount = screen.queryByText(numberOfJobs);
       expect(jobCount).not.toBeInTheDocument();
     });
