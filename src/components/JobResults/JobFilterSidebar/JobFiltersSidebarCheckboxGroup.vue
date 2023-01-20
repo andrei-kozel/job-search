62446859ca00ba736fbd374b93cdbd67704f1890
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-20 12:33:25 +0100

    checkbox group component first commit

---
+++ src/components/JobResults/JobFilterSidebar/JobFiltersSidebarCheckboxGroup.vue
@@ -0,0 +1,48 @@
+<template>
+  <collapsible-accordion class="mt-5" :header="header">
+    <fieldset>
+      <ul class="flex flex-row flex-wrap">
+        <li v-for="value in uniqueValues" :key="value" class="h-8 w-1/2">
+          <input
+            :id="value"
+            v-model="selectedValues"
+            :value="value"
+            type="checkbox"
+            class="mr-3"
+            @change="selectValue"
+          />
+          <label :for="value">{{ value }}</label>
+        </li>
+      </ul>
+    </fieldset>
+  </collapsible-accordion>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+
+const props = defineProps({
+  header: {
+    type: String,
+    required: true,
+  },
+  uniqueValues: {
+    type: Set,
+    required: true,
+  },
+  action: {
+    type: Function,
+    required: true,
+  },
+});
+
+const selectedValues = ref([]);
+
+const router = useRouter();
+const selectValue = () => {
+  props.action(selectedValues.value);
+  router.push({ name: "JobResults" });
+};
+</script>
