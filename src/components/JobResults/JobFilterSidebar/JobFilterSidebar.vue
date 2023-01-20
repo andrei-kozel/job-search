ee34b513c6ac7c3547b70e3b4708ed33ab69053b
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-20 12:44:18 +0100

    refactor using checkbox group component

--- src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
+++ src/components/JobResults/JobFilterSidebar/JobFilterSidebar.vue
@@ -9,23 +9,32 @@
           <action-button text="Clear filter" type="secondary" />
         </div>
       </div>
-      <job-filter-sidebar-organizations />
-      <job-filter-sidebar-job-types />
+      <job-filters-sidebar-checkbox-group
+        header="Job Types"
+        :unique-values="UNIQUE_ORGANIZATIONS"
+        :action="userStore.ADD_SELECTED_ORGANIZATIONS"
+      />
+      <job-filters-sidebar-checkbox-group
+        header="Organizations"
+        :unique-values="UNIQUE_JOB_TYPES"
+        :action="userStore.ADD_SELECTED_JOB_TYPES"
+      />
     </section>
   </div>
 </template>
 
-<script>
-import ActionButton from "@/components/Shared/ActionButton.vue";
-import JobFilterSidebarOrganizations from "@/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.vue";
-import JobFilterSidebarJobTypes from "@/components/JobResults/JobFilterSidebar/JobFilterSidebarJobTypes.vue";
+<script setup>
+import { computed } from "vue";
 
-export default {
-  name: "JobFilterSidebar",
-  components: {
-    ActionButton,
-    JobFilterSidebarOrganizations,
-    JobFilterSidebarJobTypes,
-  },
-};
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFilterSidebar/JobFiltersSidebarCheckboxGroup.vue";
+
+import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
+
+const jobStore = useJobsStore();
+const userStore = useUserStore();
+
+const UNIQUE_ORGANIZATIONS = computed(() => jobStore.UNIQUE_ORGANIZATIONS);
+const UNIQUE_JOB_TYPES = computed(() => jobStore.UNIQUE_JOB_TYPES);
 </script>
