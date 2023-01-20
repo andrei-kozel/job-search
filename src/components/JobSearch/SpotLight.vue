a34d92a23f6b146d6189d8ff825beeb044625743
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-20 16:40:51 +0100

    refactor using composition api

--- src/components/JobSearch/SpotLight.vue
+++ src/components/JobSearch/SpotLight.vue
@@ -10,21 +10,18 @@
   </ul>
 </template>
 
-<script>
+<script setup>
 import axios from "axios";
+import { ref, onMounted } from "vue";
 
-export default {
-  name: "SpotLight",
-  data() {
-    return {
-      spotlights: [],
-    };
-  },
-  async mounted() {
-    const baseUrl = import.meta.env.VITE_APP_API_URL;
-    const url = `${baseUrl}/spotlights`;
-    const response = await axios.get(url);
-    this.spotlights = response.data;
-  },
+const spotlights = ref([]);
+
+const getSpotlights = async () => {
+  const baseUrl = import.meta.env.VITE_APP_API_URL;
+  const url = `${baseUrl}/spotlights`;
+  const response = await axios.get(url);
+  spotlights.value = response.data;
 };
+
+onMounted(getSpotlights);
 </script>
