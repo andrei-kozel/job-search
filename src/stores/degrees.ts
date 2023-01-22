364e4e7dceb6cc7cfe348dceb8a1623f7d583120
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-22 12:44:29 +0100

    degrees store and test

---
+++ src/stores/degrees.ts
@@ -0,0 +1,8 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import type { Degree } from "@/api/types";
+
+export const useDegreesStore = defineStore("degrees", () => {
+  const degrees = ref<Degree[]>([]);
+  return { degrees };
+});
