3cc7d64fc8145c089a34a0058bb5870f2ca932e4
Andrei Kozel <kozel.andrei.94@gmail.com>
2023-01-22 21:47:13 +0100

    UNIQUE_DEGREES

--- src/stores/degrees.ts
+++ src/stores/degrees.ts
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 import type { Degree } from "@/api/types";
 import getDegrees from "@/api/getDegree";
@@ -11,5 +11,9 @@ export const useDegreesStore = defineStore("degrees", () => {
     degrees.value = resDegrees;
   };
 
-  return { degrees, FETCH_DEGREES };
+  const UNIQUE_DEGREES = computed(() =>
+    degrees.value.map((degree) => degree.degree)
+  );
+
+  return { degrees, FETCH_DEGREES, UNIQUE_DEGREES };
 });
--- tests/unit/stores/degrees.test.ts
+++ tests/unit/stores/degrees.test.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import type { Mock } from "vitest";
 
 import { useDegreesStore } from "@/stores/degrees";
+import { createDegree } from "tests/utils/createDegrees";
 
 vi.mock("axios");
 const axiosGetMock = axios.get as Mock;
@@ -32,6 +33,26 @@ describe("actions", () => {
       const store = useDegreesStore();
       await store.FETCH_DEGREES();
       expect(store.degrees).toEqual([{ id: 1, degree: "Bachelor's" }]);
+    });
+  });
+});
+
+describe("getters", () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  describe("UNIQUE_DEGREES", () => {
+    it("finds unique degrees from collection of degrees", () => {
+      const store = useDegreesStore();
+      store.degrees = [
+        createDegree({ degree: "Master's" }),
+        createDegree({ degree: "Bachelor's" }),
+      ];
+
+      const result = store.UNIQUE_DEGREES();
+
+      expect(result).toEqual(["Master's", "Bachelor's"]);
     });
   });
 });
